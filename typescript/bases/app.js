6b86c965e7f48a554a6fefc497c77d688a555f6e
MIKKEL <mikeharold077@gmail.com>
2023-10-13 21:08:43 -0600

    creacion de una carpeta para trabajar ts

---
+++ typescript/bases/app.js
@@ -0,0 +1,13 @@
+"use strict";
+let msg = 'lol';
+msg = 'lorem';
+const hero = {
+    id: 1,
+    name: 'hero',
+    powerlevel: 12,
+    powerHability: () => {
+        console.log('sing and destroy pusyes');
+    }
+};
+console.log(hero);
+hero.powerHability();
---
+++ typescript/bases/app.ts
@@ -0,0 +1,14 @@
+let msg : string = 'lol';
+msg = 'lorem'
+
+const hero ={
+    id:1,
+    name:'hero',
+    powerlevel: 12,
+    powerHability:()=>{
+        console.log('sing and destroy pusyes');
+        
+    }
+}
+console.log(hero);
+hero.powerHability()
